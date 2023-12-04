22560a3638eda0ca4088a6c458a4b04ac5d6f262
Alex Yelienok <aelenok@gmail.com>
2023-12-04 18:35:03 +0100

    Add project files.

---
+++ DZ_WF_8_12/GameForm.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace DZ_WF_8_12
+{
+    public partial class GameForm : Form
+    {
+        private int[,] map2d;
+        private int rows;
+        private int cols;
+        private int playerPosX;
+        private int playerPosY;
+        public GameForm(int[,] map2d)
+        {
+            this.map2d = map2d;
+            rows = map2d.GetLength(0);
+            cols = map2d.GetLength(1);
+            playerPosX = rows / 2;
+            playerPosY = cols / 2;
+
+            Load += (s, e) =>
+            {
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+                MaximizeBox = false;
+
+                Paint += GameForm_Paint;
+                KeyDown += GameForm_KeyDown;
+
+                DoubleBuffered = true;
+            };
+
+            InitializeComponent();
+        }
+        private void GameForm_KeyDown(object? sender, KeyEventArgs e)
+        {
+            int x = playerPosX, y = playerPosY;
+
+            if (e.KeyCode == Keys.Up)
+            {
+                y = playerPosY - 1;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                y = playerPosY + 1;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                x = playerPosX - 1;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                x = playerPosX + 1;
+            }
+
+            if (x != playerPosX || y != playerPosY)
+            {
+                if (x < cols && y < rows && map2d[y, x] == 0)
+                {
+                    playerPosX = x; playerPosY = y;
+                    Invalidate();
+
+                    if (playerPosX == cols - 1 || playerPosY == rows - 1 || playerPosX == 0 || playerPosY == 0)
+                    {
+                        MessageBox.Show("Перемога!");
+                        Close();
+                    }
+                }
+            }
+        }
+        private void GameForm_Paint(object? sender, PaintEventArgs e)
+        {
+            ClientSize = new Size(rows * cols, rows * cols);
+
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < map2d.GetLength(0); ++i)
+            {
+                for (int j = 0; j < map2d.GetLength(1); j++)
+                {
+                    Brush brush = map2d[i, j] == 1 ? Brushes.Gray : Brushes.Chartreuse;
+                    g.FillRectangle(brush, j * cols, i * rows, rows, cols);
+                }
+            }
+            g.FillEllipse(Brushes.Crimson, playerPosX * rows, playerPosY * cols, rows, cols);
+        }
+    }
+}
---
+++ DZ_WF_8_12/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows.Forms;
+
+namespace DZ_WF_8_12
+{
+    public partial class MainForm : Form
+    {
+        private string mapPath;
+        private readonly string fileExtension = ".txt";
+        private List<string> maps;
+        private int[,] map2d;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            maps = new List<string>();
+            LoadMaps();
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            Load += (s, e) =>
+            {
+                startButton.Enabled = false;
+                InitMapSelection();
+                mapSelectionComboBox.SelectedIndexChanged += MapSelectionComboBox_SelectedIndexChanged;
+                startButton.Click += (s, e) =>
+                {
+                    GameForm gameForm = new GameForm(map2d);
+                    gameForm.ShowDialog(this);
+                };
+            };
+        }
+
+        private void MapSelectionComboBox_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            if (mapSelectionComboBox.SelectedItem != null)
+            {
+                string selectedMapName = mapSelectionComboBox.SelectedItem.ToString();
+                string filePath = Path.Combine(mapPath, selectedMapName + fileExtension);
+
+                try
+                {
+                    LoadMapFromFile(filePath);
+                    DisplayMap();
+                    EnableStartButton();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error reading file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LoadMapFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            map2d = new int[lines.Length, lines[0].Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    map2d[i, j] = lines[i][j] == '█' ? 1 : 0;
+                }
+            }
+        }
+
+        private void DisplayMap()
+        {
+            int rows = map2d.GetLength(0);
+            int cols = map2d.GetLength(1);
+            int cellWidth = mapPictureBox.Width / cols;
+            int cellHeight = mapPictureBox.Height / rows;
+
+            Bitmap mapImage = new Bitmap(mapPictureBox.Width, mapPictureBox.Height);
+
+            using (Graphics g = Graphics.FromImage(mapImage))
+            {
+                g.Clear(Color.Gray);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        Brush brush = map2d[i, j] == 1 ? Brushes.Gray : Brushes.Chartreuse;
+                        g.FillRectangle(brush, j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                    }
+                }
+            }
+            mapPictureBox.Image = mapImage;
+        }
+
+        private void InitMapSelection()
+        {
+            foreach (string map in maps)
+            {
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(map);
+                mapSelectionComboBox.Items.Add(fileNameWithoutExtension);
+            }
+        }
+
+        private void EnableStartButton()
+        {
+            startButton.Enabled = true;
+            startButton.Text = "Старт гри";
+        }
+
+        private void LoadMaps()
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Оберіть папку розташування карт";
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    mapPath = folderBrowserDialog.SelectedPath;
+                    string[] files = Directory.GetFiles(mapPath, "*.txt");
+
+                    foreach (string file in files)
+                    {
+                        string fileName = Path.GetFileName(file);
+                        maps.Add(fileName);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid folder selection. The application will close.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
